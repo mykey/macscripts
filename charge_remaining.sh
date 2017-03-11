c959842c288f6d0b38c52eb1b9cfb9440dde3bc5
Michael Cox <mr.michaelcox@gmail.com>
2017-03-12 00:46:47 +0100

    forgotten shebang

--- charge_remaining.sh
+++ charge_remaining.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 remaining=$(system_profiler SPPowerDataType | grep "Charge Remaining" | awk '{print $4}e')
 capacity=$(system_profiler SPPowerDataType | grep "Charge Capacity" | awk '{print $5}e')
 pct=$(pmset -g batt | grep "Internal" | awk '{print $3}e')
