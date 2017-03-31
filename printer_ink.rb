3900457c91bd99d2260b82208c8b88d2389aa9db
Michael Cox <mr.michaelcox@gmail.com>
2017-03-31 14:22:50 +0200

    update syntax

--- printer_ink.rb
+++ printer_ink.rb
@@ -1,13 +1,12 @@
 #!/usr/local/bin/ruby
 # Ruby script to check ink level from our corporate printer
 
-require "open-uri"
-require "Nokogiri"
+require 'open-uri'
+require 'Nokogiri'
 
 # Load the printer's web interface and store everything in the data variable for further processing
 
-printer_url ="http://10.96.58.251/main.asp?Lang=en-us"
-
+printer_url = 'http://10.96.58.251/main.asp?Lang=en-us'
 data = Nokogiri::HTML(open(printer_url))
 
 # Get the different values for ink/messages/... and store them in variables
@@ -16,16 +15,16 @@ black = data.css('tr:nth-child(3) .settingCategoryL11')
 magenta = data.css('tr:nth-child(4) .settingCategoryL11')
 yellow = data.css('tr:nth-child(5) .settingCategoryL11')
 cyan = data.css('tr:nth-child(6) .settingCategoryL11')
-#wastebottle =
-#transferunit =
-#fusingunit =
-#roller =
-#paper1 =
-#paperhand =
+# wastebottle =
+# transferunit =
+# fusingunit =
+# roller =
+# paper1 =
+# paperhand =
 
 # Display the gathered info
 
-#puts data
+# puts data
 puts black
 puts magenta
 puts yellow
